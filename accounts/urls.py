40f798992a6e946f8775ef6da94e1b65bfaab7f5
sapkalesanghkirti-commits <sapkalesanghkirti@gmail.com>
2026-03-08 15:51:40 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
@@ -12,3 +17,6 @@ urlpatterns = [
     path('follow/', views.follow),
     path('unfollow/', views.unfollow)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
