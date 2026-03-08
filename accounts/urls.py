0d68791b0b74259deeeb553f13eeea3fd9a22413
sapkalesanghkirti-commits <sapkalesanghkirti@gmail.com>
2026-03-08 16:40:44 +0530

    mda

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,6 +17,3 @@ urlpatterns = [
     path('follow/', views.follow),
     path('unfollow/', views.unfollow)
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
